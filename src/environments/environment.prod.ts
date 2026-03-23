52ea52ced1d02979c8dc7af7a074fe0426d88db0
Marvin Seb <msebcu@gmail.com>
2026-03-23 01:44:13 -0600

    feat: configure production environment and firebase hostings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.task-atom.com',
+  apiUrl: 'https://api-r6akj2neoa-uc.a.run.app/api',
   http: {
     defaultRetries: 2,
     defaultTimeout: 20000,
